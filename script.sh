e644f7a2c04138a2ce09f245afdcddd4e492d78f
dashan <dagaoaa@connect.ust.hk>
2021-06-23 14:03:11 +0800

    script

---
+++ script.sh
@@ -0,0 +1,1 @@
+python main.py --dataset mnist_rot --f_pretrain True --save_step 100 --g_optimizer adam --g_deeper_layer True --g_base_channel_dim 8 --img_size 32 --adv_loss_lambda 0.1 --g_msp_lambda 0.0001 --g_vae_lambda 0.0 --g_rec_lambda 0.001 --comment Rece-3Adv01Mspe-4
